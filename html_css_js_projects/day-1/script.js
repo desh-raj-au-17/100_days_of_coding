b716c876757ad9409e019daae60bd92e7b811c55
desh-raj-au-17 <fullstackdev318@gmail.com>
2022-01-08 11:14:15 +0530

    #100daysOfCode-day_1

---
+++ html_css_js_projects/day-1/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
+
+const btn = buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add")) {
+    count.innerHTML++;
+    setColor();
+  }
+  if (e.target.classList.contains("subtract")) {
+    count.innerHTML--;
+    setColor();
+  }
+  if (e.target.classList.contains("reset")) {
+    count.innerHTML = 0;
+    setColor();
+  }
+});
+
+function setColor() {
+  if (count.innerHTML > 0) {
+    count.style.color = "green";
+  } else if (count.innerHTML < 0) {
+    count.style.color = "red";
+  } else {
+    count.style.color = "#fff";
+  }
+}
